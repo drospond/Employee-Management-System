5a67208cc3103bb39fd059d38e64dc7c15c99d21
drospond <david.rospond@gmail.com>
2020-04-07 16:55:24 -0400

    function added to view employees and roles

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const cTable = require('console.table');
 
 function startInterface(){
     inquirer.prompt([
@@ -12,8 +13,10 @@ function startInterface(){
     ]).then((res)=>{
         switch(res.action){
             case "View employees":
+                getEmployees();
                 break;
             case "View roles":
+                getRoles();
                 break;
             case "View departments":
                 break;
@@ -25,11 +28,15 @@ function startInterface(){
                 break;
             case "Update an employee's role":
                 break;
+            default:
+                console.log("invalid input")
+                startInterface();
+                break;
         }
     })
 }
 
-//startInterface();
+startInterface();
 
 //////
 //MySQL queries
@@ -45,3 +52,23 @@ connection.connect(function(err){
     if(err) throw err;
     console.log("Connected: " + connection.state);
 })
+
+function getEmployees(){
+    connection.query(`SELECT e1.id, e1.first_name, e1.last_name, departments.department_name, roles.title, roles.salary, concat(e2.first_name, " ", e2.last_name) as manager_name FROM employees e1 LEFT JOIN employees e2 ON e1.manager_id = e2.id JOIN roles ON e1.role_id = roles.id JOIN departments ON departments.id = roles.department_id;`, 
+    (err, data)=>{
+        if(err) console.log(err);
+        console.table(data);
+    })
+}
+
+function getRoles(){
+    connection.query("SELECT roles.title, departments.department_name, roles.salary FROM roles JOIN departments ON departments.id = roles.department_id;",
+    (err, data)=>{
+        if(err) console.log(err);
+        console.table(data);
+    })
+}
+
+//to figure out:
+    //starting interface after table in console
+    //creating DB
--- seed.sql
+++ seed.sql
@@ -35,4 +35,4 @@ INSERT INTO roles (title, salary, department_id)
 VALUES ("Head of Sales", 50000, 1), ("Head of Accounting", 45000, 2), ("Branch Manager", 50001, 3);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("Jim","Halpert", 1, 3), ("Oscar","Martinez", 2, 3), ("Michael","Scott", 3, 3);
+VALUES ("Jim","Halpert", 1, 3), ("Oscar","Martinez", 2, 3), ("Michael","Scott", 3, null);
