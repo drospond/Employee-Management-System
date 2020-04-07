62c6bb4c7e8f59d3d0d05ee11de0ca1fe63a20fc
drospond <david.rospond@gmail.com>
2020-04-07 18:11:59 -0400

    created addRole and addDepartment functions

--- index.js
+++ index.js
@@ -8,26 +8,32 @@ function startInterface(){
             type: "list",
             name: "action",
             message: "What would you like to do?",
-            choices: ["View employees", "View roles", "View departments", "Add employee", "Add role", "Add department", "Update an employee's role"]
+            choices: ["View employees", "View roles", "View departments", "Add employee", "Add role", "Add department", "Update an employee's role", "Quit"]
         }
     ]).then((res)=>{
         switch(res.action){
             case "View employees":
-                getEmployees();
+                viewEmployees();
                 break;
             case "View roles":
-                getRoles();
+                viewRoles();
                 break;
             case "View departments":
+                viewDepartments();
                 break;
             case "Add employee":
+                addEmployee();
                 break;
             case "Add role":
+                addRole();
                 break;
             case "Add department":
+                addDepartment();
                 break;
             case "Update an employee's role":
                 break;
+            case "Quit":
+                process.exit();
             default:
                 console.log("invalid input")
                 startInterface();
@@ -53,7 +59,7 @@ connection.connect(function(err){
     console.log("Connected: " + connection.state);
 })
 
-function getEmployees(){
+function viewEmployees(){
     connection.query(`SELECT e1.id, e1.first_name, e1.last_name, departments.department_name, roles.title, roles.salary, concat(e2.first_name, " ", e2.last_name) as manager_name FROM employees e1 LEFT JOIN employees e2 ON e1.manager_id = e2.id JOIN roles ON e1.role_id = roles.id JOIN departments ON departments.id = roles.department_id;`, 
     (err, data)=>{
         if(err) console.log(err);
@@ -61,14 +67,86 @@ function getEmployees(){
     })
 }
 
-function getRoles(){
-    connection.query("SELECT roles.title, departments.department_name, roles.salary FROM roles JOIN departments ON departments.id = roles.department_id;",
+function viewRoles(){
+    connection.query("SELECT roles.id, roles.title, departments.department_name, roles.salary FROM roles JOIN departments ON departments.id = roles.department_id;",
     (err, data)=>{
         if(err) console.log(err);
         console.table(data);
     })
 }
 
+function viewDepartments(){
+    connection.query("SELECT * FROM departments;", (err, data)=>{
+        if(err) console.log(err);
+        console.table(data);
+    })
+}
+
+function addDepartment(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "department",
+            message: "What is the name of the new department?"
+        }
+    ]).then((res)=>connection.query(`INSERT INTO departments(department_name) VALUE (?);`, res.department));
+}
+
+function addRole(){
+    connection.query(`SELECT department_name, id FROM departments;`, (err, data)=>{
+        if(err) console.log(err);
+        departmentsArray = data.map(row=> row.department_name);
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "role",
+                message: "What is the name of the new role?"
+            },
+            {
+                type: "list",
+                name: "department",
+                message: "What department is the role in?",
+                choices: departmentsArray
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "What is the salary of this role?"
+            }
+        ]).then((res)=>{
+            const departmentId = data.filter(row => row.department_name === res.department);
+            connection.query("INSERT INTO roles(title, salary, department_id) VALUE (?,?,?)",[res.role, Number(res.salary),departmentId[0].id]);
+        })
+    })
+}
+
+function addEmployee(){
+    connection.query(`SELECT department_name, id FROM departments;`, (err, departmentData)=>{
+        if(err) console.log(err);
+        departmentsArray = departmentData.map(row=> row.department_name);
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "role",
+                message: "What is the name of the new role?"
+            },
+            {
+                type: "list",
+                name: "department",
+                message: "What department is the role in?",
+                choices: departmentsArray
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "What is the salary of this role?"
+            }
+        ]).then((res)=>{
+            const departmentId = departmentData.filter(row => row.department_name === res.department);
+            connection.query("INSERT INTO roles(title, salary, department_id) VALUE (?,?,?)",[res.role, Number(res.salary),departmentId[0].id]);
+        })
+    })
+}
 //to figure out:
     //starting interface after table in console
     //creating DB
