192a15077d49ff4b85f0a3684c6efac8a31aa1c7
drospond <david.rospond@gmail.com>
2020-04-07 15:33:22 -0400

    MySQL connection established

--- index.js
+++ index.js
@@ -29,4 +29,19 @@ function startInterface(){
     })
 }
 
-startInterface();
+//startInterface();
+
+//////
+//MySQL queries
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "employee_trackerDB"
+})
+
+connection.connect(function(err){
+    if(err) throw err;
+    console.log("Connected: " + connection.state);
+})
